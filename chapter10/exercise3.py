dbbf65ea762b08b32243dc630f18e357292509b7
romwil <romwil.pilapil@gmail.com>
2020-12-21 02:26:08 +0800

    update code chapter 10, exercise3

--- chapter10/exercise3.py
+++ chapter10/exercise3.py
@@ -7,14 +7,50 @@ letter frequency varies between languages. Compare your results with
 the tables at https://wikipedia.org/wiki/Letter_frequencies."""
 
 import string
-fileread = open('mbox-short.txt')
+fileread = None
 
+# User input file text
+while True:
+    user_input = input('Enter file name: ')
+    try:
+        fileread = open(user_input)
+        print()
+        break
+    except:
+        print(user_input, 'invalid file name, try again')
+        print()
+
+# dictionary for letters
+letters_dict = dict()
+
+# Read the user file text
 for line in fileread:
     line = line.rstrip()
     line = line.lower()
-    line = line = line.translate(str.maketrans('', '', string.punctuation))
+    line = line.translate(str.maketrans('', '', string.punctuation))
     splitline = line.split()
+
     if not len(splitline) >= 1:
         continue
-    print(splitline)
-    print(len(splitline))
+
+    for index in splitline:
+        lst_char = list(index)  # List of character
+        for char in lst_char:
+            try:
+                integer = int(char)
+            except:
+                letters_dict[char] = letters_dict.get(char, 0) + 1
+
+# List of letters and values
+letters_list = list()
+
+# Add the dictionaries items to List of items
+for letter, count in letters_dict.items():
+    letters_list.append((count, letter))
+
+# Sort variable "letters_list" to decending order
+letters_list.sort(reverse=True)
+
+# Print the list of items in decreasing  order
+for count, letter in letters_list:
+    print(letter + ':', count)
